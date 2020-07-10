ec85d474f363880f6ebd9efee32faf06fdf3dc95
shuklayash <shuklayash00001@gmail.com>
2020-07-10 10:25:42 +0530

    Compilation stages rat maze cpp .ii .o .s

---
+++ ratmazebtself.s
@@ -0,0 +1,555 @@
+	.file	"ratmazebtself.cpp"
+	.text
+	.section	.rodata
+	.type	_ZStL19piecewise_construct, @object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.type	_ZStL13allocator_arg, @object
+	.size	_ZStL13allocator_arg, 1
+_ZStL13allocator_arg:
+	.zero	1
+	.type	_ZStL6ignore, @object
+	.size	_ZStL6ignore, 1
+_ZStL6ignore:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,1
+	.align 4
+	.type	_ZN9__gnu_cxxL21__default_lock_policyE, @object
+	.size	_ZN9__gnu_cxxL21__default_lock_policyE, 4
+_ZN9__gnu_cxxL21__default_lock_policyE:
+	.long	2
+	.type	_ZStL10defer_lock, @object
+	.size	_ZStL10defer_lock, 1
+_ZStL10defer_lock:
+	.zero	1
+	.type	_ZStL11try_to_lock, @object
+	.size	_ZStL11try_to_lock, 1
+_ZStL11try_to_lock:
+	.zero	1
+	.type	_ZStL10adopt_lock, @object
+	.size	_ZStL10adopt_lock, 1
+_ZStL10adopt_lock:
+	.zero	1
+	.align 4
+	.type	_ZNSt15regex_constantsL5icaseE, @object
+	.size	_ZNSt15regex_constantsL5icaseE, 4
+_ZNSt15regex_constantsL5icaseE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL6nosubsE, @object
+	.size	_ZNSt15regex_constantsL6nosubsE, 4
+_ZNSt15regex_constantsL6nosubsE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL8optimizeE, @object
+	.size	_ZNSt15regex_constantsL8optimizeE, 4
+_ZNSt15regex_constantsL8optimizeE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL7collateE, @object
+	.size	_ZNSt15regex_constantsL7collateE, 4
+_ZNSt15regex_constantsL7collateE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL10ECMAScriptE, @object
+	.size	_ZNSt15regex_constantsL10ECMAScriptE, 4
+_ZNSt15regex_constantsL10ECMAScriptE:
+	.long	16
+	.align 4
+	.type	_ZNSt15regex_constantsL5basicE, @object
+	.size	_ZNSt15regex_constantsL5basicE, 4
+_ZNSt15regex_constantsL5basicE:
+	.long	32
+	.align 4
+	.type	_ZNSt15regex_constantsL8extendedE, @object
+	.size	_ZNSt15regex_constantsL8extendedE, 4
+_ZNSt15regex_constantsL8extendedE:
+	.long	64
+	.align 4
+	.type	_ZNSt15regex_constantsL3awkE, @object
+	.size	_ZNSt15regex_constantsL3awkE, 4
+_ZNSt15regex_constantsL3awkE:
+	.long	128
+	.align 4
+	.type	_ZNSt15regex_constantsL4grepE, @object
+	.size	_ZNSt15regex_constantsL4grepE, 4
+_ZNSt15regex_constantsL4grepE:
+	.long	256
+	.align 4
+	.type	_ZNSt15regex_constantsL5egrepE, @object
+	.size	_ZNSt15regex_constantsL5egrepE, 4
+_ZNSt15regex_constantsL5egrepE:
+	.long	512
+	.align 4
+	.type	_ZNSt15regex_constantsL12__polynomialE, @object
+	.size	_ZNSt15regex_constantsL12__polynomialE, 4
+_ZNSt15regex_constantsL12__polynomialE:
+	.long	1024
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_defaultE, @object
+	.size	_ZNSt15regex_constantsL13match_defaultE, 4
+_ZNSt15regex_constantsL13match_defaultE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_bolE, @object
+	.size	_ZNSt15regex_constantsL13match_not_bolE, 4
+_ZNSt15regex_constantsL13match_not_bolE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_eolE, @object
+	.size	_ZNSt15regex_constantsL13match_not_eolE, 4
+_ZNSt15regex_constantsL13match_not_eolE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_bowE, @object
+	.size	_ZNSt15regex_constantsL13match_not_bowE, 4
+_ZNSt15regex_constantsL13match_not_bowE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL13match_not_eowE, @object
+	.size	_ZNSt15regex_constantsL13match_not_eowE, 4
+_ZNSt15regex_constantsL13match_not_eowE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL9match_anyE, @object
+	.size	_ZNSt15regex_constantsL9match_anyE, 4
+_ZNSt15regex_constantsL9match_anyE:
+	.long	16
+	.align 4
+	.type	_ZNSt15regex_constantsL14match_not_nullE, @object
+	.size	_ZNSt15regex_constantsL14match_not_nullE, 4
+_ZNSt15regex_constantsL14match_not_nullE:
+	.long	32
+	.align 4
+	.type	_ZNSt15regex_constantsL16match_continuousE, @object
+	.size	_ZNSt15regex_constantsL16match_continuousE, 4
+_ZNSt15regex_constantsL16match_continuousE:
+	.long	64
+	.align 4
+	.type	_ZNSt15regex_constantsL16match_prev_availE, @object
+	.size	_ZNSt15regex_constantsL16match_prev_availE, 4
+_ZNSt15regex_constantsL16match_prev_availE:
+	.long	128
+	.align 4
+	.type	_ZNSt15regex_constantsL14format_defaultE, @object
+	.size	_ZNSt15regex_constantsL14format_defaultE, 4
+_ZNSt15regex_constantsL14format_defaultE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL10format_sedE, @object
+	.size	_ZNSt15regex_constantsL10format_sedE, 4
+_ZNSt15regex_constantsL10format_sedE:
+	.long	256
+	.align 4
+	.type	_ZNSt15regex_constantsL14format_no_copyE, @object
+	.size	_ZNSt15regex_constantsL14format_no_copyE, 4
+_ZNSt15regex_constantsL14format_no_copyE:
+	.long	512
+	.align 4
+	.type	_ZNSt15regex_constantsL17format_first_onlyE, @object
+	.size	_ZNSt15regex_constantsL17format_first_onlyE, 4
+_ZNSt15regex_constantsL17format_first_onlyE:
+	.long	1024
+	.align 4
+	.type	_ZNSt15regex_constantsL13error_collateE, @object
+	.size	_ZNSt15regex_constantsL13error_collateE, 4
+_ZNSt15regex_constantsL13error_collateE:
+	.zero	4
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_ctypeE, @object
+	.size	_ZNSt15regex_constantsL11error_ctypeE, 4
+_ZNSt15regex_constantsL11error_ctypeE:
+	.long	1
+	.align 4
+	.type	_ZNSt15regex_constantsL12error_escapeE, @object
+	.size	_ZNSt15regex_constantsL12error_escapeE, 4
+_ZNSt15regex_constantsL12error_escapeE:
+	.long	2
+	.align 4
+	.type	_ZNSt15regex_constantsL13error_backrefE, @object
+	.size	_ZNSt15regex_constantsL13error_backrefE, 4
+_ZNSt15regex_constantsL13error_backrefE:
+	.long	3
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_brackE, @object
+	.size	_ZNSt15regex_constantsL11error_brackE, 4
+_ZNSt15regex_constantsL11error_brackE:
+	.long	4
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_parenE, @object
+	.size	_ZNSt15regex_constantsL11error_parenE, 4
+_ZNSt15regex_constantsL11error_parenE:
+	.long	5
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_braceE, @object
+	.size	_ZNSt15regex_constantsL11error_braceE, 4
+_ZNSt15regex_constantsL11error_braceE:
+	.long	6
+	.align 4
+	.type	_ZNSt15regex_constantsL14error_badbraceE, @object
+	.size	_ZNSt15regex_constantsL14error_badbraceE, 4
+_ZNSt15regex_constantsL14error_badbraceE:
+	.long	7
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_rangeE, @object
+	.size	_ZNSt15regex_constantsL11error_rangeE, 4
+_ZNSt15regex_constantsL11error_rangeE:
+	.long	8
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_spaceE, @object
+	.size	_ZNSt15regex_constantsL11error_spaceE, 4
+_ZNSt15regex_constantsL11error_spaceE:
+	.long	9
+	.align 4
+	.type	_ZNSt15regex_constantsL15error_badrepeatE, @object
+	.size	_ZNSt15regex_constantsL15error_badrepeatE, 4
+_ZNSt15regex_constantsL15error_badrepeatE:
+	.long	10
+	.align 4
+	.type	_ZNSt15regex_constantsL16error_complexityE, @object
+	.size	_ZNSt15regex_constantsL16error_complexityE, 4
+_ZNSt15regex_constantsL16error_complexityE:
+	.long	11
+	.align 4
+	.type	_ZNSt15regex_constantsL11error_stackE, @object
+	.size	_ZNSt15regex_constantsL11error_stackE, 4
+_ZNSt15regex_constantsL11error_stackE:
+	.long	12
+	.align 8
+	.type	_ZNSt8__detailL19_S_invalid_state_idE, @object
+	.size	_ZNSt8__detailL19_S_invalid_state_idE, 8
+_ZNSt8__detailL19_S_invalid_state_idE:
+	.quad	-1
+.LC0:
+	.string	" "
+.LC1:
+	.string	"\n"
+	.text
+	.globl	_Z13printSolutionPA4_i
+	.type	_Z13printSolutionPA4_i, @function
+_Z13printSolutionPA4_i:
+.LFB8188:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	$0, -8(%rbp)
+.L5:
+	cmpl	$3, -8(%rbp)
+	jg	.L6
+	movl	$0, -4(%rbp)
+.L4:
+	cmpl	$3, -4(%rbp)
+	jg	.L3
+	movl	-8(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	cltq
+	movl	(%rdx,%rax,4), %eax
+	movl	%eax, %esi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZNSolsEi@PLT
+	leaq	.LC0(%rip), %rsi
+	movq	%rax, %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	addl	$1, -4(%rbp)
+	jmp	.L4
+.L3:
+	leaq	.LC1(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+	addl	$1, -8(%rbp)
+	jmp	.L5
+.L6:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8188:
+	.size	_Z13printSolutionPA4_i, .-_Z13printSolutionPA4_i
+	.globl	_Z6isSafePA4_iii
+	.type	_Z6isSafePA4_iii, @function
+_Z6isSafePA4_iii:
+.LFB8189:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movl	%edx, -16(%rbp)
+	movl	-12(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-8(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-16(%rbp), %eax
+	cltq
+	movl	(%rdx,%rax,4), %eax
+	cmpl	$1, %eax
+	jne	.L8
+	cmpl	$0, -12(%rbp)
+	js	.L8
+	cmpl	$0, -16(%rbp)
+	js	.L8
+	cmpl	$3, -12(%rbp)
+	jg	.L8
+	cmpl	$3, -16(%rbp)
+	jg	.L8
+	movl	$1, %eax
+	jmp	.L9
+.L8:
+	movl	$0, %eax
+.L9:
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8189:
+	.size	_Z6isSafePA4_iii, .-_Z6isSafePA4_iii
+	.globl	_Z9solveMazePA4_iiiS0_
+	.type	_Z9solveMazePA4_iiiS0_, @function
+_Z9solveMazePA4_iiiS0_:
+.LFB8190:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -8(%rbp)
+	movl	%esi, -12(%rbp)
+	movl	%edx, -16(%rbp)
+	movq	%rcx, -24(%rbp)
+	movl	-12(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-8(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-16(%rbp), %eax
+	cltq
+	movl	(%rdx,%rax,4), %eax
+	cmpl	$1, %eax
+	jne	.L11
+	cmpl	$3, -12(%rbp)
+	jne	.L11
+	cmpl	$3, -16(%rbp)
+	jne	.L11
+	movl	-12(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-16(%rbp), %eax
+	cltq
+	movl	$1, (%rdx,%rax,4)
+	movl	$1, %eax
+	jmp	.L12
+.L11:
+	movl	-16(%rbp), %edx
+	movl	-12(%rbp), %ecx
+	movq	-8(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
+	call	_Z6isSafePA4_iii
+	testb	%al, %al
+	je	.L13
+	movl	-12(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-16(%rbp), %eax
+	cltq
+	movl	$1, (%rdx,%rax,4)
+	movl	-12(%rbp), %eax
+	leal	1(%rax), %esi
+	movq	-24(%rbp), %rcx
+	movl	-16(%rbp), %edx
+	movq	-8(%rbp), %rax
+	movq	%rax, %rdi
+	call	_Z9solveMazePA4_iiiS0_
+	testb	%al, %al
+	je	.L14
+	movl	$1, %eax
+	jmp	.L12
+.L14:
+	movl	-16(%rbp), %eax
+	leal	1(%rax), %edi
+	movq	-24(%rbp), %rdx
+	movl	-12(%rbp), %esi
+	movq	-8(%rbp), %rax
+	movq	%rdx, %rcx
+	movl	%edi, %edx
+	movq	%rax, %rdi
+	call	_Z9solveMazePA4_iiiS0_
+	testb	%al, %al
+	je	.L15
+	movl	$1, %eax
+	jmp	.L12
+.L15:
+	movl	-12(%rbp), %eax
+	cltq
+	salq	$4, %rax
+	movq	%rax, %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-16(%rbp), %eax
+	cltq
+	movl	$0, (%rdx,%rax,4)
+	movl	$0, %eax
+	jmp	.L12
+.L13:
+	movl	$0, %eax
+.L12:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8190:
+	.size	_Z9solveMazePA4_iiiS0_, .-_Z9solveMazePA4_iiiS0_
+	.section	.rodata
+.LC2:
+	.string	"No Solution"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB8191:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$144, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	movl	$1, -144(%rbp)
+	movl	$0, -140(%rbp)
+	movl	$0, -136(%rbp)
+	movl	$0, -132(%rbp)
+	movl	$1, -128(%rbp)
+	movl	$1, -124(%rbp)
+	movl	$0, -120(%rbp)
+	movl	$1, -116(%rbp)
+	movl	$0, -112(%rbp)
+	movl	$1, -108(%rbp)
+	movl	$0, -104(%rbp)
+	movl	$0, -100(%rbp)
+	movl	$1, -96(%rbp)
+	movl	$1, -92(%rbp)
+	movl	$1, -88(%rbp)
+	movl	$1, -84(%rbp)
+	movq	$0, -80(%rbp)
+	movq	$0, -72(%rbp)
+	movq	$0, -64(%rbp)
+	movq	$0, -56(%rbp)
+	movq	$0, -48(%rbp)
+	movq	$0, -40(%rbp)
+	movq	$0, -32(%rbp)
+	movq	$0, -24(%rbp)
+	leaq	-80(%rbp), %rdx
+	leaq	-144(%rbp), %rax
+	movq	%rdx, %rcx
+	movl	$0, %edx
+	movl	$0, %esi
+	movq	%rax, %rdi
+	call	_Z9solveMazePA4_iiiS0_
+	testb	%al, %al
+	je	.L17
+	leaq	-80(%rbp), %rax
+	movq	%rax, %rdi
+	call	_Z13printSolutionPA4_i
+	jmp	.L18
+.L17:
+	leaq	.LC2(%rip), %rsi
+	leaq	_ZSt4cout(%rip), %rdi
+	call	_ZStlsISt11char_traitsIcEERSt13basic_ostreamIcT_ES5_PKc@PLT
+.L18:
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L20
+	call	__stack_chk_fail@PLT
+.L20:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE8191:
+	.size	main, .-main
+	.type	_Z41__static_initialization_and_destruction_0ii, @function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB9501:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L23
+	cmpl	$65535, -8(%rbp)
+	jne	.L23
+	leaq	_ZStL8__ioinit(%rip), %rdi
+	call	_ZNSt8ios_base4InitC1Ev@PLT
+	leaq	__dso_handle(%rip), %rdx
+	leaq	_ZStL8__ioinit(%rip), %rsi
+	movq	_ZNSt8ios_base4InitD1Ev@GOTPCREL(%rip), %rax
+	movq	%rax, %rdi
+	call	__cxa_atexit@PLT
+.L23:
+	nop
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9501:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.type	_GLOBAL__sub_I__Z13printSolutionPA4_i, @function
+_GLOBAL__sub_I__Z13printSolutionPA4_i:
+.LFB9524:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movl	$65535, %esi
+	movl	$1, %edi
+	call	_Z41__static_initialization_and_destruction_0ii
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE9524:
+	.size	_GLOBAL__sub_I__Z13printSolutionPA4_i, .-_GLOBAL__sub_I__Z13printSolutionPA4_i
+	.section	.init_array,"aw"
+	.align 8
+	.quad	_GLOBAL__sub_I__Z13printSolutionPA4_i
+	.hidden	__dso_handle
+	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
+	.section	.note.GNU-stack,"",@progbits
